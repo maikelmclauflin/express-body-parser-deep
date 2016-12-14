bc2702169df799e731fa4d9784eebcb7944d1ce2
mmclau14 <michael.j.mclaughlin20@gmail.com>
2016-12-14 11:32:25 -0600

    added basic code and package for npm

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = {
+    json: function (req, res, next) {
+        var n, body = req.body;
+        for (n in body) {
+            body[n] = parse(body[n]);
+        }
+        next();
+    }
+};
+
+function parse(value) {
+    var first = value[0],
+        last = value[value.length - 1];
+    if ((first === '{' && last === '}') || (first === '[' && last === ']')) {
+        return tryparse(value);
+    } else {
+        return value;
+    }
+}
+
+function tryparse(value) {
+    try {
+        return JSON.parse(value);
+    } catch (e) {}
+}
