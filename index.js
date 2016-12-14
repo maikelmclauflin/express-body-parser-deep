1e8b34718dcae71ae4b8289caf5e40fa0de3f8c0
mmclau14 <michael.j.mclaughlin20@gmail.com>
2016-12-14 11:59:47 -0600

    updated bodyparser index with usable code for express. needed to test somewhere

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 module.exports = {
-    json: function (req, res, next) {
-        var n, body = req.body;
-        for (n in body) {
-            body[n] = parse(body[n]);
-        }
-        next();
+    json: function () {
+        return function (req, res, next) {
+            var n, body = req.body;
+            for (n in body) {
+                body[n] = parse(body[n]);
+            }
+            next();
+        };
     }
 };
 
 function parse(value) {
+    if (typeof value !== 'string') {
+        return value;
+    }
     var first = value[0],
         last = value[value.length - 1];
     if ((first === '{' && last === '}') || (first === '[' && last === ']')) {
